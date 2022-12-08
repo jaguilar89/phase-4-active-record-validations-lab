f1cd8f99476100b054397c0cf6387ec9e75c9c28
Jose Aguilar <jaguilarny@gmail.com>
2022-12-08 16:59:47 -0500

    commit tree fabcbcc72b0260ca2c5620ce661092fd50af8fc3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    words = ["Won't Believe", "Secret", "Top", "Guess"]
+
+    match = words.any? do |word|
+      title.to_s.include?(word)
+    end
+
+    unless match
+      errors.add(:title, "Title not allowed!")
+    end
+  end
 end
+
+=begin
+    1. Title must contain the selected words
+    2. Add a validation error if the title does not contain ALL of those words.
+=end
